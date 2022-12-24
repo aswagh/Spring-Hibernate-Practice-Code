9667adfa2cefd9d15193a339952b2d9b826a7e08
Atul Wagh <aswagh303@gmail.com>
2022-12-24 16:09:57 +0530

    Srping Demo One

---
+++ SpringDemoOne/src/SpringDemoOne.java
@@ -0,0 +1,19 @@
+/**
+this is simple project for adding library files of Spring framework 5
+ */
+
+/**
+ * @author atulw
+ *
+ */
+public class SpringDemoOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Main function");
+	}
+
+}
